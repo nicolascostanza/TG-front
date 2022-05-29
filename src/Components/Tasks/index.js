002571183b3d29b9c857d1cc7024eaf99b8b1bdc
Martin Pueblas <war.pueblas@gmail.com>
2022-05-29 11:42:04 -0300

    TG-70: set up created & data received

--- src/Components/Tasks/index.js
+++ src/Components/Tasks/index.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from 'react';
 import styles from './tasks.module.css';
 
 function Tasks() {
+  // eslint-disable-next-line no-unused-vars
+  const [task, setTasks] = useState([]);
+  useEffect(async () => {
+    try {
+      const response = await fetch('http://localhost:8080/tasks');
+      const data = await response.json();
+      console.log(data);
+      setTasks(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <section className={styles.container}>
       <h2>Tasks</h2>
