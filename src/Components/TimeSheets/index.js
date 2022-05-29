d2484ab69c0f8b61c341d4341ce0a8760ca512cb
JuanBSantomero <juanbautista.santomero@gmail.com>
2022-05-29 01:30:36 -0300

    TG-75: Branch created

--- src/Components/TimeSheets/index.js
+++ src/Components/TimeSheets/index.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from 'react';
 import styles from './time-sheets.module.css';
 
 function TimeSheets() {
+  // eslint-disable-next-line no-unused-vars
+  const [timeSheet, saveTimeSheets] = useState([]);
+  useEffect(async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/time-sheets`);
+      const data = await response.json();
+      console.log(data);
+      saveTimeSheets(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <section className={styles.container}>
       <h2>TimeSheets</h2>
