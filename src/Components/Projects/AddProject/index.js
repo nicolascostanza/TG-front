eb0aa8de5977f02069f113a254281938e94e27d6
Usuario <martinalbor96@gmail.com>
2022-05-31 00:45:13 -0300

    TG-66: Add project feature

--- src/Components/Projects/AddProject/index.js
+++ src/Components/Projects/AddProject/index.js
@@ -1,46 +1,117 @@
-// import { useState } from 'react';
-import React from 'react';
+import { useState } from 'react';
+import addProjectStyles from './addProject.module.css';
 
 const AddProject = () => {
-  // const [projectInput, setProjectInput] = useState(props)
+  const initialValues = {
+    name: '',
+    description: '',
+    clientName: '',
+    startDate: '',
+    endDate: '',
+    projectManager: '',
+    team: [],
+    tasks: []
+  };
+
+  const [project, setProject] = useState(initialValues);
+
+  const handleInputChanges = (e) => {
+    const { name, value } = e.target;
+    setProject({
+      ...project,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:8080/projects/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // mode: 'no-cors',
+      body: JSON.stringify({
+        name: project.name,
+        description: project.description,
+        clientName: project.clientName,
+        startDate: project.startDate,
+        endDate: project.endDate,
+        projectManager: project.projectManager,
+        team: project.team,
+        tasks: project.tasks
+      })
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (!json.error) {
+          console.log('hello mfs');
+          resetValues();
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const resetValues = () => {
+    setProject(initialValues);
+  };
+
   return (
-    <form>
+    <form className={addProjectStyles.form}>
       <label>
-        Project name:
-        <input type="text" />
+        Name:
+        <input value={project.name} onChange={handleInputChanges} name="name" type="text" />
       </label>
       <label>
         Description:
-        <input type="text" />
+        <input
+          value={project.description}
+          onChange={handleInputChanges}
+          name="description"
+          type="text"
+        />
       </label>
       <label>
-        Client name:
-        <input type="text" />
+        Client Name:
+        <input
+          value={project.clientName}
+          onChange={handleInputChanges}
+          name="clientName"
+          type="text"
+        />
       </label>
       <label>
         Start Date:
-        <input type="date" />
+        <input
+          value={project.startDate}
+          onChange={handleInputChanges}
+          name="startDate"
+          type="date"
+        />
       </label>
       <label>
-        End date:
-        <input type="date" />
+        End Date:
+        <input value={project.endDate} onChange={handleInputChanges} name="endDate" type="date" />
       </label>
       <label>
-        Project manager:
-        <input type="text" />
+        Project Manager:
+        <input
+          value={project.projectManager}
+          onChange={handleInputChanges}
+          name="projectManager"
+          type="text"
+        />
       </label>
       <label>
         Team:
-        <input type="text" />
+        <input value={project.team} onChange={handleInputChanges} name="team" type="text" />
       </label>
       <label>
         Tasks:
-        <input type="text" />
+        <input value={project.tasks} onChange={handleInputChanges} name="tasks" type="text" />
       </label>
-      <label>
-        Admin:
-        <input type="text" />
-      </label>
+      <button onClick={resetValues}>RESET</button>
+      <button onClick={handleSubmit}>CREATE</button>
     </form>
   );
 };
---
+++ src/Components/Projects/List/ListItem/EmployeesDropdown/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const EmployeesDropdown = () => {
+  // const { employees } = props;
+  return <p>incoming feature</p>;
+};
+
+export default EmployeesDropdown;
