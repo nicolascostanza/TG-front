53e288fda535b76b724840bcd8d24d6241816d8c
JuanBSantomero <juanbautista.santomero@gmail.com>
2022-05-31 20:35:02 -0300

    TG-75: Styles for add and edit

--- src/Components/TimeSheets/Edit/index.js
+++ src/Components/TimeSheets/Edit/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import styles from '../Add/Form.module.css';
 
 const params = new URLSearchParams(window.location.search);
 const timeSheetId = params.get('id');
@@ -69,83 +70,85 @@ function EditTimeSheets() {
     setRole('');
   };
   return (
-    <form onSubmit={onSubmit}>
-      <div>
+    <form className={styles.form} onSubmit={onSubmit}>
+      <div className={styles.tittle}>
         <h2> Edit Time-Sheet </h2>
       </div>
-      <div>
-        <label> Employee ID </label>
-        <input
-          type="text"
-          placeholder="Employee ID"
-          value={employeeId}
-          onChange={(e) => setEmployeeId(e.target.value)}
-        />
+      <div className={styles.container}>
+        <div>
+          <label> Employee ID </label>
+          <input
+            type="text"
+            placeholder="Employee ID"
+            value={employeeId}
+            onChange={(e) => setEmployeeId(e.target.value)}
+          />
+        </div>
+        <div>
+          <label> Description </label>
+          <input
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label> Project </label>
+          <input
+            type="text"
+            placeholder="Project"
+            value={project}
+            onChange={(e) => setProject(e.target.value)}
+          />
+        </div>
+        <div>
+          <label> Date </label>
+          <input
+            type="text"
+            placeholder="Date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+        <div>
+          <label> Hours </label>
+          <input
+            type="number"
+            placeholder="Hours"
+            value={hours}
+            onChange={(e) => setHours(e.target.value)}
+          />
+        </div>
+        <div>
+          <label> Task </label>
+          <input
+            type="text"
+            placeholder="Task"
+            value={task._id}
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </div>
+        <div>
+          <label> Approved </label>
+          <input
+            type="checkbox"
+            // checked={approved}
+            value={approved}
+            onChange={(e) => setApproved(e.target.value)}
+          />
+        </div>
+        <div>
+          <label> Role </label>
+          <input
+            type="text"
+            placeholder="Role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          />
+        </div>
       </div>
-      <div>
-        <label> Description </label>
-        <input
-          type="text"
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </div>
-      <div>
-        <label> Project </label>
-        <input
-          type="text"
-          placeholder="Project"
-          value={project}
-          onChange={(e) => setProject(e.target.value)}
-        />
-      </div>
-      <div>
-        <label> Date </label>
-        <input
-          type="text"
-          placeholder="Date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-      </div>
-      <div>
-        <label> Hours </label>
-        <input
-          type="number"
-          placeholder="Hours"
-          value={hours}
-          onChange={(e) => setHours(e.target.value)}
-        />
-      </div>
-      <div>
-        <label> Task </label>
-        <input
-          type="text"
-          placeholder="Task"
-          value={task._id}
-          onChange={(e) => setTask(e.target.value)}
-        />
-      </div>
-      <div>
-        <label> Approved </label>
-        <input
-          type="checkbox"
-          // checked={approved}
-          value={approved}
-          onChange={(e) => setApproved(e.target.value)}
-        />
-      </div>
-      <div>
-        <label> Role </label>
-        <input
-          type="text"
-          placeholder="Role"
-          value={role}
-          onChange={(e) => setRole(e.target.value)}
-        />
-      </div>
-      <input type="submit" value="Edit Time-Sheet" />
+      <input className={styles.button} type="submit" value="Edit Time-Sheet" />
     </form>
   );
 }
