7800395ea181d178a8e08130ac93a6bdae9b4b6e
Federico Troanes <ftroanes@gmail.com>
2022-05-30 21:31:32 -0300

    TG-68: Redirection to Add form added

---
+++ src/Components/Employees/AddBtn/AddBtn.js
@@ -0,0 +1,9 @@
+const AddBtn = () => {
+  return (
+    <a href="http://localhost:3000/employees/addEmployee">
+      <button>Add</button>
+    </a>
+  );
+};
+
+export default AddBtn;
--- src/Components/Employees/index.js
+++ src/Components/Employees/index.js
@@ -1,11 +1,13 @@
 // import { useEffect, useState } from 'react';
 import styles from './employees.module.css';
 import List from './List/List';
+import AddBtn from './AddBtn/AddBtn';
 
 function Employees() {
   return (
     <section className={styles.container}>
       <h2>Employees</h2>
+      <AddBtn />
       <List />
     </section>
   );
