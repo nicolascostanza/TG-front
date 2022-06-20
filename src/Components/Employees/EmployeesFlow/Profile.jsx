d905973ec912ebe2e72cc7b0e90ebed83afe9256
GinaSchP <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>
2022-06-20 01:51:29 -0300

    TG-123: validations on signUp, styles on Profile

--- src/Components/Employees/EmployeesFlow/Profile.jsx
+++ src/Components/Employees/EmployeesFlow/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import styles from './profile.module.css';
 
 function Profile() {
   const param = useParams();
@@ -44,9 +45,12 @@ function Profile() {
   return (
     <>
       <h1>Welcome {employee.firstName}</h1>
-      <form>
+      <button className={styles.edit} onClick={() => setUpdate(!update)}>
+        EDIT
+      </button>
+      <form className={styles.form}>
         <div>
-          <label>Name</label>
+          <label className={styles.label}>Name</label>
           {update ? (
             <p>{employee.firstName}</p>
           ) : (
@@ -59,16 +63,118 @@ function Profile() {
             ></input>
           )}
         </div>
-        <span>{employee.lastName}</span>
-        <span>{employee.email}</span>
-        <span>{employee.gender}</span>
-        {/* cambiar el adress con 2 d */}
-        <span>{employee.adress}</span>
-        {/* <span>{new Date(employee.dob).toISOString().split('T')[0]}</span> */}
-        <span>{employee.active ? 'true' : 'false'}</span>
-        <span>{employee.phone}</span>
+        <div>
+          <label className={styles.label}>Last Name</label>
+          {update ? (
+            <p>{employee.lastName}</p>
+          ) : (
+            <input
+              type="text"
+              name="email"
+              disabled={update}
+              value={employee.lastName}
+              onChange={handleInputChanges}
+            ></input>
+          )}
+        </div>
+        <div>
+          <label className={styles.label}>Email</label>
+          {update ? (
+            <p>{employee.email}</p>
+          ) : (
+            <input
+              type="text"
+              name="email"
+              disabled={update}
+              value={employee.email}
+              onChange={handleInputChanges}
+            ></input>
+          )}
+        </div>
+        <div>
+          <label className={styles.label}>Password</label>
+          {update ? (
+            <p>{employee.password}</p>
+          ) : (
+            <input
+              type="text"
+              name="password"
+              disabled={update}
+              value={employee.password}
+              onChange={handleInputChanges}
+            ></input>
+          )}
+        </div>
+        <div>
+          <label className={styles.label}>Gender</label>
+          {update ? (
+            <p>{employee.gender}</p>
+          ) : (
+            <select>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </select>
+          )}
+        </div>
+        <div>
+          <label className={styles.label}>Address</label>
+          {update ? (
+            <p>{employee.adress}</p>
+          ) : (
+            <input
+              type="text"
+              name="address"
+              disabled={update}
+              value={employee.adress}
+              onChange={handleInputChanges}
+            ></input>
+          )}
+        </div>
+        <div>
+          <label className={styles.label}>Phone</label>
+          {update ? (
+            <p>{employee.phone}</p>
+          ) : (
+            <input
+              type="text"
+              name="phone"
+              disabled={update}
+              value={employee.phone}
+              onChange={handleInputChanges}
+            ></input>
+          )}
+        </div>
+        <div>
+          <label className={styles.label}>Date Of Birth</label>
+          {update ? (
+            ''
+          ) : (
+            // <p>{new Date(employee.dob).toISOString().split('T')[0]}</p>
+            <input
+              type="date"
+              name="date"
+              disabled={update}
+              value={employee.dob}
+              onChange={handleInputChanges}
+            ></input>
+          )}
+        </div>
+        <div>
+          <label className={styles.label}>Active</label>
+          {update ? (
+            <p>{employee.active ? 'true' : 'false'}</p>
+          ) : (
+            <input
+              type="checkbox"
+              name="active"
+              disabled={update}
+              value={employee.active}
+              onChange={handleInputChanges}
+            ></input>
+          )}
+        </div>
       </form>
-      <button onClick={() => setUpdate(!update)}>EDIT</button>
     </>
   );
 }
--- src/Components/Employees/EmployeesFlow/SignUp.jsx
+++ src/Components/Employees/EmployeesFlow/SignUp.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../../Shared/Sidebar';
 import styles from './signup.module.css';
-import Button from '../../Shared/Button/Button';
+// import Button from '../../Shared/Button';
+import { appendErrors, useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import validations from './validations';
+import Modal from '../../Shared/Modal';
+import * as thunks from '../../../redux/employees/thunks';
+import { useDispatch, useSelector } from 'react-redux';
 
 function SignUp() {
-  const [firstName, setFirstName] = useState('');
-  const [surname, setSurname] = useState('');
-  const [email, setEmail] = useState('');
-  const [gender, setGender] = useState('');
-  const [adress, setAdress] = useState('');
-  const [dob, setDob] = useState('');
-  const [password, setPassword] = useState('');
-  const [phone, setPhone] = useState('');
-  const [active, setActive] = useState('');
-
-  const valueGenderChange = (e) => {
-    return setGender(e.target.value);
+  const message = useSelector((state) => state.employees.message);
+  const error = useSelector((state) => state.employees.response);
+  const dispatch = useDispatch();
+  const [showModalMessage, setShowModalMessage] = useState(false);
+  const handleCloseMessage = () => {
+    setShowModalMessage(false);
+    setShowModalMessage('');
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(validations)
+  });
+
+  console.log(errors);
+
+  const onSubmit = (data) => {
+    dispatch(thunks.addEmployee({ data }));
+    if (!error) {
+      reset;
+    }
+    setShowModalMessage(true);
+
     console.log('on submit');
+    console.log(data);
   };
 
   return (
@@ -29,101 +47,87 @@ function SignUp() {
       <section>
         <Sidebar />
       </section>
+      <Modal showModal={showModalMessage} handleClose={handleCloseMessage}>
+        <p>{message}</p>
+      </Modal>
       <section className={styles.form}>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <h1>Sign Up</h1>
           <div>
             <label>First name</label>
             <input
               type="text"
-              name="firstName"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              {...register('firstName')}
+              error={appendErrors.firstName?.message}
             ></input>
+            {errors.firstName && <p className={styles.errorInput}>{errors.firstName?.message}</p>}
           </div>
           <div>
             <label>Surname</label>
             <input
               type="text"
-              name="surname"
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
+              {...register('surName')}
+              error={appendErrors.surName?.message}
             ></input>
+            {errors.surName && <p className={styles.errorInput}>{errors.surName?.message}</p>}
           </div>
           <div>
             <label>Email</label>
-            <input
-              type="text"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
+            <input type="text" {...register('email')} error={appendErrors.email?.message}></input>
+            {errors.email && <p className={styles.errorInput}>{errors.email?.message}</p>}
           </div>
           <div>
             <label>Gender</label>
-            <select value={gender} onChange={valueGenderChange}>
+            <select {...register('gender')}>
               <option value="Male">Male</option>
               <option value="Female">Female</option>
               <option value="Other">Other</option>
             </select>
+            {errors.gender && <p className={styles.errorInput}>{errors.gender?.message}</p>}
           </div>
           <div>
             <label>Address</label>
             <input
               type="text"
-              name="adress"
-              value={adress}
-              onChange={(e) => setAdress(e.target.value)}
+              {...register('address')}
+              error={appendErrors.address?.message}
             ></input>
+            {errors.address && <p className={styles.errorInput}>{errors.address?.message}</p>}
           </div>
           <div>
             <label>Dob</label>
-            <input
-              type="date"
-              name="dob"
-              value={dob}
-              onChange={(e) => setDob(e.target.value)}
-            ></input>
+            <input type="date" {...register('dob')} error={appendErrors.dob?.message}></input>
+            {errors.dob && <p className={styles.errorInput}>{errors.dob?.message}</p>}
           </div>
           <div>
             <label>Password</label>
             <input
               type="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              {...register('password')}
+              error={appendErrors.password?.message}
             ></input>
+            {errors.password && <p className={styles.errorInput}>{errors.password?.message}</p>}
           </div>
           <div>
             <label>Phone</label>
-            <input
-              type="number"
-              name="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            ></input>
+            <input type="text" {...register('phone')} error={appendErrors.phone?.message}></input>
+            {errors.phone && <p className={styles.errorInput}>{errors.phone?.message}</p>}
           </div>
           <div>
             <div>
               <label>Active</label>
             </div>
             <div>
-              <input
-                className={styles.checkbox}
-                type="checkbox"
-                checked={active}
-                value={active}
-                onChange={(e) => setActive(e.currentTarget.checked)}
-              />
+              <input className={styles.checkbox} type="checkbox" {...register('active')} />
             </div>
             <div className={styles.button}>
-              <Button onClick={''} width={'100px'} height={'35px'} fontSize={'15px'}>
+              {/* <Button width={'100px'} height={'35px'} fontSize={'15px'}>
                 send
-              </Button>
+              </Button> */}
+              <div className={styles.button}>
+                <input type="submit" value="Send" />
+              </div>
             </div>
-            {/* <div className={styles.button}>
-              <input type="submit" value="Send" />
-            </div> */}
           </div>
         </form>
       </section>
