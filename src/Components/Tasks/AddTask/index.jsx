cfca4f0f06779dc35941ac8555499f397d1e6504
Martin Pueblas <war.pueblas@gmail.com>
2022-06-21 18:06:00 -0300

    BaSP-TG-129: Hook form for create task

--- src/Components/Tasks/AddTask/index.jsx
+++ src/Components/Tasks/AddTask/index.jsx
@@ -1,108 +1,102 @@
 import React from 'react';
-import { useState } from 'react';
 import styles from './addTask.module.css';
-import Dropdown from '../../Shared/Dropdown';
+import Joi from 'joi';
+import { joiResolver } from '@hookform/resolvers/joi';
 import Form from '../../Shared/Form';
 import * as thunks from '../../../redux/tasks/thunks';
 import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+
+const schema = Joi.object({
+  parentProject: Joi.string()
+    .alphanum()
+    .required()
+    .messages({ 'string.empty': 'This field is required' }),
+  taskName: Joi.string().min(1).max(50).required().messages({
+    'string.min': 'Name must contain 1 or more characters',
+    'string.max': 'Name must contain 50 or less characters',
+    'string.empty': 'This field is required'
+  }),
+  taskDescription: Joi.string().min(1).max(250).optional().messages({
+    'string.min': 'Name must contain 1 or more characters',
+    'string.max': 'Name must contain 250 or less characters'
+  }),
+  assignedEmployee: Joi.string(),
+  startDate: Joi.date().required().messages({
+    'string.empty': 'This field is required',
+    'date.base': 'This must be a valid date'
+  }),
+  status: Joi.required().messages({ 'string.empty': 'This field is required' })
+});
 
 const AddTask = (props) => {
-  const [parentProject, setParentProject] = useState('');
-  const [taskName, setTaskName] = useState('');
-  const [taskDescription, setTaskDescription] = useState('');
-  const [assignedEmployee, setAssignedEmployee] = useState([]);
-  const [startDate, setStartDate] = useState('');
-  const [status, setStatus] = useState('');
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({
+    mode: 'OnBlur',
+    resolver: joiResolver(schema)
+  });
+
   const dispatch = useDispatch();
+  console.log(errors);
 
   const addTask = async (task) => {
     dispatch(thunks.addTask(task));
     props.handleClose();
   };
-  const onSubmit = (e) => {
-    e.preventDefault();
-    addTask({
-      parentProject,
-      taskName,
-      taskDescription,
-      assignedEmployee: [assignedEmployee],
-      startDate,
-      status
-    });
-    setParentProject('');
-    setTaskName('');
-    setTaskDescription('');
-    setAssignedEmployee([]);
-    setStartDate('');
-    setStatus('');
+  const onSubmit = (data) => {
+    console.log('data: ', data);
+    addTask(data);
   };
-
-  const valueChange = (e) => {
-    return setStatus(e.target.value);
-  };
-
   return (
-    <Form showModal={props.showAddModal} handleClose={props.handleClose} handleSubmit={onSubmit}>
+    <Form
+      showModal={props.showAddModal}
+      handleClose={props.handleClose}
+      handleSubmit={handleSubmit(onSubmit)}
+    >
       <div>
         <h2>Add New Task</h2>
       </div>
       <div className={styles.form}>
         <div>
-          <label>Parent Project:</label>
-          <input
-            type="text"
-            placeholder="Parent Project ID"
-            value={parentProject}
-            // onBlur={(e) => handleError(e.target.value)}
-            onChange={(e) => setParentProject(e.target.value)}
-          />
+          <label htmlFor="parentProject">Parent Project:</label>
+          <input type="text" placeholder="Parent Project ID" {...register('parentProject')} />
+          {errors.parentProject?.type === 'string.empty' && <p>{errors.parentProject.message}</p>}
         </div>
         <div>
-          <label>Task Name:</label>
-          <input
-            type="text"
-            placeholder="Task Name"
-            value={taskName}
-            onChange={(e) => setTaskName(e.target.value)}
-          />
+          <label htmlFor="taskName">Task Name:</label>
+          <input type="text" placeholder="Task Name" {...register('taskName')} />
+          {errors.taskName?.type === 'string.empty' && <p>{errors.taskName.message}</p>}
+          {errors.taskName?.type === 'string.min' && <p>{errors.taskName.message}</p>}
+          {errors.taskName?.type === 'string.max' && <p>{errors.taskName.message}</p>}
         </div>
         <div>
-          <label>Task Description:</label>
-          <input
-            type="text"
-            placeholder="Task description"
-            value={taskDescription}
-            onChange={(e) => setTaskDescription(e.target.value)}
-          />
+          <label htmlFor="taskDescription">Task Description:</label>
+          <input type="text" placeholder="Task description" {...register('taskDescription')} />
+          {errors.taskDescription?.type === 'string.min' && <p>{errors.taskDescription.message}</p>}
+          {errors.taskDescription?.type === 'string.max' && <p>{errors.taskDescription.message}</p>}
         </div>
         <div>
-          <label>Assigned Employee:</label>
-          <input
-            type="text"
-            placeholder="Assigned Employee ID"
-            value={assignedEmployee}
-            onChange={(e) => setAssignedEmployee(e.target.value)}
-          />
+          <label htmlFor="assignedEmployee">Assigned Employee:</label>
+          <input type="text" placeholder="Assigned Employee ID" {...register('assignedEmployee')} />
+          {errors.assignedEmployee?.type === 'array.base' && (
+            <p>{errors.assignedEmployee.message}</p>
+          )}
         </div>
         <div>
-          <label>Start Date:</label>
-          <input
-            type="text"
-            placeholder="YYYY-MM-DD"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-          />
+          <label htmlFor="startDate">Start Date:</label>
+          <input type="text" placeholder="YYYY-MM-DD" {...register('startDate')} />
+          {errors.startDate?.type === 'string.empty' && <p>{errors.startDate.message}</p>}
+          {errors.startDate?.type === 'date.base' && <p>{errors.startDate.message}</p>}
         </div>
         <div className={styles.dropdown}>
-          <Dropdown
-            title="Status"
-            value={status}
-            onChange={valueChange}
-            placeholder="Choose an option"
-          >
+          <label htmlFor="status">Status</label>
+          <select {...register('status')}>
             <option value="Ready to deliver">Ready to deliver</option>
             <option value="Paused">Paused</option>
-          </Dropdown>
+          </select>
         </div>
       </div>
     </Form>
