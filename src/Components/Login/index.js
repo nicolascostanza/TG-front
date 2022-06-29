b37051bb0c8004d4d8ccb295581fb2bbedf78a83
martinalbrnz <martinalbor96@gmail.com>
2022-06-29 15:23:47 -0300

    TG-138: Login bug solved, redirect if authenticated

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -3,13 +3,14 @@ import { appendErrors, useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
 import { employeeValidationLogIn } from 'Components/EmployeesFlow/validations';
 import * as thunksAuth from 'redux/auth/thunks';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Sidebar from 'Components/Shared/Sidebar';
 import styles from './login.module.css';
 import { useHistory } from 'react-router-dom';
 
 const Login = () => {
   const history = useHistory();
+  const role = useSelector((state) => state.auth.authenticated);
   const dispatch = useDispatch();
   const {
     handleSubmit,
@@ -19,9 +20,14 @@ const Login = () => {
     mode: 'onBlur',
     resolver: joiResolver(employeeValidationLogIn)
   });
+
   const onSubmit = (data) => {
     dispatch(thunksAuth.login(data));
   };
+
+  if (role) {
+    history.push('/');
+  }
   return (
     <section className={styles.container}>
       <section>
@@ -32,20 +38,18 @@ const Login = () => {
           <h1 className={styles.tittle}>LOGIN</h1>
           <div className={styles.formFlex}>
             <div className={styles.inputContainer}>
-              <label htmlFor="Email" className={styles.labels}></label>
-              <input
-                type="text"
-                {...register('email')}
-                placeholder="Email"
-                error={appendErrors.email?.message}
-              ></input>
+              <label htmlFor="Email" className={styles.labels}>
+                Email
+              </label>
+              <input type="text" {...register('email')} error={appendErrors.email?.message}></input>
               {errors.email && <p className={styles.errorInput}>{errors.email?.message}</p>}
             </div>
             <div className={styles.inputContainer}>
-              <label htmlFor="Password" className={styles.labels}></label>
+              <label htmlFor="Password" className={styles.labels}>
+                Password
+              </label>
               <input
                 type="password"
-                placeholder="Password"
                 {...register('password')}
                 error={appendErrors.password?.message}
               ></input>
@@ -53,12 +57,7 @@ const Login = () => {
             </div>
           </div>
           <div className={styles.buttonsContainer}>
-            <button
-              className={styles.buttonContinue}
-              type="submit"
-              value="CONTINUE"
-              onClick={() => history.push('/employees/profile/629d83d3d9d731ead71b218c')}
-            >
+            <button className={styles.buttonContinue} type="submit" value="CONTINUE">
               CONTINUE
             </button>
           </div>
