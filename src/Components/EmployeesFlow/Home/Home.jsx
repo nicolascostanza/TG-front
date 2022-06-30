11403f0a02d723a71b82c401ca91039c667ecbc9
martinalbrnz <martinalbor96@gmail.com>
2022-06-30 14:38:45 -0300

    TG-138: Fix functionality of Timesheet list on employee home

--- src/Components/EmployeesFlow/Home/Home.jsx
+++ src/Components/EmployeesFlow/Home/Home.jsx
@@ -3,7 +3,8 @@ import styles from './home.module.css';
 import { useEffect } from 'react';
 import * as Timesheetsthunks from 'redux/timesheets/thunks';
 import { useDispatch, useSelector } from 'react-redux';
-import Table from 'Components/Shared/Table';
+// import Table from 'Components/Shared/Table';
+import TimeSheets from 'Components/TimeSheets';
 import { useHistory } from 'react-router-dom';
 import Sidebar from 'Components/Shared/Sidebar';
 
@@ -11,7 +12,6 @@ function Home() {
   const history = useHistory();
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.currentUser.currentUser);
-  const timeSheets = useSelector((state) => state.timesheet.list);
   useEffect(() => {
     dispatch(Timesheetsthunks.getTimesheets());
   }, []);
@@ -27,21 +27,7 @@ function Home() {
         </button>
       </div>
       <div className={styles.table}>
-        <Table
-          title="Timesheets"
-          headers={[
-            '_id',
-            'employeeId',
-            'description',
-            'project',
-            'date',
-            'task_name',
-            'hours',
-            'approved',
-            'role'
-          ]}
-          data={timeSheets}
-        />
+        <TimeSheets />
       </div>
     </>
   );
