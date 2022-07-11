cbd30a6381512326c9a5f6df20e41ad32a0d260f
Karen Agustina Soto <karenagustinams@gmail.com>
2022-07-11 00:25:04 -0300

    BaSP-QA: Sign Up test added

--- test/pageobjects/signup.page.js
+++ test/pageobjects/signup.page.js
@@ -1,15 +1,15 @@
 class SignUpPage {
     ///// GETTERS /////
     // Inputs
-    get registerTitle() { return $('#root > div > section > section.signup_form__e\+PH4 > form > h1') }
-    get firstNameInput() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(2) > div:nth-child(1) > input[type=text]') }
-    get lastNameInput() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(2) > div:nth-child(2) > input[type=text]') }
-    get genderInput() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(3) > div:nth-child(1) > select') }
-    get addressInput() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(3) > div:nth-child(2) > input[type=text]') }
-    get birthDateInput() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(4) > div:nth-child(1) > input[type=date]') }
-    get phoneInput() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(4) > div:nth-child(2) > input[type=text]') }
-    get registerEmailInput() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(5) > div:nth-child(1) > input[type=text]') }
-    get registerPasswordInput() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(5) > div:nth-child(2) > input[type=password]') }
+    get registerTitle() { return $('form > h1') }
+    get firstNameInput() { return $('[name="firstName"]') }
+    get lastNameInput() { return $('[name="lastName"]') }
+    get genderInput() { return $('[name="gender"]') }
+    get addressInput() { return $('[name="address"]') }
+    get birthDateInput() { return $('[name="dob"]') }
+    get phoneInput() { return $('[name="phone"]') }
+    get registerEmailInput() { return $('[name="email"]') }
+    get registerPasswordInput() { return $('[name="password"]') }
     // Error messages
     get fistNameError() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(2) > div:nth-child(1) > p') }
     get lastNameError() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(2) > div:nth-child(2) > p') }
@@ -20,20 +20,20 @@ class SignUpPage {
     get registerEmailError() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(5) > div:nth-child(1) > p') }
     get registerPasswordError() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div:nth-child(5) > div:nth-child(2) > p') }
     // Buttons
-    get resetBtn() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div.signup_buttonsContainer__RXc4V > button.signup_buttonReset__zwjfO') }
-    get continueBtn() { return $('#root > div > section > section.signup_form__e\+PH4 > form > div.signup_buttonsContainer__RXc4V > button.signup_buttonContinue__hlqTE') }
-
+    get resetBtn() { return $('.signup_buttonReset__zwjfO') }
+    get continueBtn() { return $('.signup_buttonContinue__hlqTE') }
+    //
+    get employeeCreatedMsg() { return $('#root > div > section > div > div > div.modal_modalBody__IaBTo > div > p') }
     ///// SETTERS /////
     async register(firstname, lastname, gender, address, birthdate, phone, email, password) {
     await this.firstNameInput.setValue(firstname)
     await this.lastNameInput.setValue(lastname)
-    await this.genderInput.setValue(gender)
+    await this.genderInput.selectByIndex(gender)
     await this.addressInput.setValue(address)
     await this.birthDateInput.setValue(birthdate)
     await this.phoneInput.setValue(phone)
     await this.registerEmailInput.setValue(email)
     await this.registerPasswordInput.setValue(password)
-    await this.continueBtn.click()
     }
 }
 module.exports = new SignUpPage();
--- test/specs/signup.spec.js
+++ test/specs/signup.spec.js
@@ -7,7 +7,22 @@ describe('REGISTER page test', () => {
         await HomePage.sidebarBtn.click()
         await HomePage.signUpLink.click()
     })
-    it('Should redirect to REGISTER page', async () => {
-        await expect(browser).toHaveUrl('https://alfonso-trackgenix-app.vercel.app/signup')
+    describe('Checking elements to be displayed', () => {
+        it('Should redirect to REGISTER page', async () => {
+            await expect(browser).toHaveUrl('https://alfonso-trackgenix-app.vercel.app/signup')
+        })
+        it('Verifying if the title of the page is correct', async () => {
+            await expect(browser).toHaveTitle('TRACKgenix')
+        })
+        it('Should displayed REGISTER title on form', async () => {
+            await expect(SignUpPage.registerTitle).toHaveText('REGISTER')
+        })
+    })
+    describe('Checking REGISTER form', () => {
+        it('Sending valid data, should display a modal with an employee created message', async () => {
+            await SignUpPage.register('Karen','Soto','2','Coco 1234','20/11/1995','3417120979','prueba1@gmail.com','asdf1234')
+            await SignUpPage.continueBtn.click()
+            await expect(SignUpPage.employeeCreatedMsg).toBePresent()
+        })
     })
 })
