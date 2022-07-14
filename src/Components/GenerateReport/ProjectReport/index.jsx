90fe9e4f89737059da8b3c087092abbf6de93547
martinalbrnz <martinalbor96@gmail.com>
2022-07-14 16:16:34 -0300

    TG-157: Add condensed table data structure

--- src/Components/GenerateReport/ProjectReport/index.jsx
+++ src/Components/GenerateReport/ProjectReport/index.jsx
@@ -12,7 +12,7 @@ const ProjectReport = () => {
   const projectTimesheets = useSelector((state) => state.timesheet.listFromProject);
   const isProjectsFetching = useSelector((state) => state.projects.isFetching);
   const isTimesheetsFetching = useSelector((state) => state.timesheet.isFetching);
-  const projectId = '62c377590aedc7c1fac7e897'; // DELETE THIS
+  const projectId = '62c377590aedc7c1fac7e897'; // DELETE THIS LATER
   useEffect(() => {
     dispatch(projectsThunks.getProjects());
     dispatch(timesheetsThunks.getTimesheetsFromProject(projectId, null));
@@ -20,8 +20,8 @@ const ProjectReport = () => {
   }, []);
 
   const project = projects?.find((project) => project._id === projectId);
-  // console.log('team:', project.team);
 
+  // Basic table formatter
   const projectTimesheetsMap = projectTimesheets
     .sort((a, b) => new Date(a.date) - new Date(b.date))
     .map((ts) => {
@@ -29,9 +29,27 @@ const ProjectReport = () => {
       const rate = ts.projectId.team.find((item) => item.employeeId === id).rate; // what a mess..
       return {
         ...ts,
+        date: new Date(ts.date).toISOString().split('T')[0],
         rate
       };
     });
+
+  // Segmented by employee
+  const segmentedByEmployee = project?.team.map((member) => {
+    const hours = projectTimesheets
+      .filter((ts) => {
+        return ts.employeeId._id === member.employeeId._id;
+      })
+      .reduce((prev, curr) => {
+        return prev + curr.hours;
+      }, 0);
+    return {
+      ...member.employeeId,
+      rate: member.rate,
+      hours,
+      totalRate: hours * member.rate
+    };
+  });
 
   const totalHours = projectTimesheets.reduce((prev, curr) => {
     return prev + curr.hours;
@@ -42,11 +60,17 @@ const ProjectReport = () => {
   }, 0);
 
   if (isProjectsFetching || isTimesheetsFetching) {
-    return <Loader />;
+    return <Loader isLoading={true} />;
   }
   return (
     <div className={styles.reportContainer}>
       <h2>{`${project?.name}`}</h2>
+      <p>
+        From{' '}
+        {`${projectTimesheetsMap[0].date} to ${
+          projectTimesheetsMap[projectTimesheetsMap.length - 1].date
+        }`}
+      </p>
       <table>
         <thead>
           <tr>
@@ -62,7 +86,7 @@ const ProjectReport = () => {
             // const employeeId = timesheet.employeeId._id;
             return (
               <tr key={timesheet._id}>
-                <td>{new Date(timesheet.date).toISOString().split('T')[0]}</td>
+                <td>{timesheet.date}</td>
                 <td>{timesheet.hours}</td>
                 <td>{timesheet.employeeId.firstName}</td>
                 <td>{timesheet.rate}</td>
@@ -80,6 +104,28 @@ const ProjectReport = () => {
             <td>{totalRate}</td>
           </tr>
         </tfoot>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Employee</th>
+            <th>Total Hours</th>
+            <th>Hourly Rate</th>
+            <th>Total Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {segmentedByEmployee?.map((employee) => {
+            return (
+              <tr key={`${employee._id}segemp`}>
+                <td>{employee.firstName}</td>
+                <td>{employee.hours}</td>
+                <td>{employee.rate}</td>
+                <td>{employee.rate * employee.hours}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
