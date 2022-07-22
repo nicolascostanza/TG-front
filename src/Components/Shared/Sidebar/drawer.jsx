6df10a927530020cb6fb786dddb702ed3a14b75d
ilcosme <fer.c@live.com.ar>
2022-07-22 01:54:21 -0300

    sidebar implementation testing with role

--- src/Components/Shared/Sidebar/drawer.jsx
+++ src/Components/Shared/Sidebar/drawer.jsx
@@ -2,34 +2,41 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 //import Button from '@mui/material/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import MailIcon from '@mui/icons-material/Mail';
 import { IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import * as authActions from 'redux/auth/actions';
+import * as currentUserActions from 'redux/currentUser/actions';
 
-export default function TemporaryDrawer() {
+function TemporaryDrawer() {
+  const dispatch = useDispatch();
+  const logOut = () => {
+    dispatch(authActions.setAuthentication(false));
+    dispatch(currentUserActions.setCurrentUserToInitialState());
+  };
+  const role = useSelector((state) => state.auth.authenticated?.role);
   const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false
+    left: false
   });
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
+  const toggleDrawer = (anchor, open) => () => {
+    // if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    //   return;
+    // }
 
     setState({ ...state, [anchor]: open });
   };
 
-  const list = (anchor) => (
+  const listWithNoRole = (anchor) => (
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
       role="presentation"
@@ -37,17 +44,38 @@ export default function TemporaryDrawer() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+          <ListItem key={text} disablePadding={false}>
             <ListItemButton>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
-        ))}
+        ))} */}
+        <ListItem disablePadding={false}>
+          <Link to={'/'}>
+            <ListItemButton>
+              <ListItemText primary={'Home'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to={'/signup'}>
+            <ListItemButton>
+              <ListItemText primary={'Sign Up'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to={'/login'}>
+            <ListItemButton>
+              <ListItemText primary={'Log In'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
       </List>
       <Divider />
-      <List>
+      {/* <List>
         {['All mail', 'Trash', 'Spam'].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
@@ -56,30 +84,184 @@ export default function TemporaryDrawer() {
             </ListItemButton>
           </ListItem>
         ))}
-      </List>
+      </List> */}
     </Box>
   );
 
-  return (
-    <div>
-      {['left'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
-          <IconButton
-            onClick={toggleDrawer(anchor, true)}
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
-    </div>
+  const listEmp = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+          <ListItem key={text} disablePadding={false}>
+            <ListItemButton>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))} */}
+        <ListItem disablePadding={false}>
+          <Link to={'/'}>
+            <ListItemButton>
+              <ListItemText primary={'Home'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding={false}>
+          <Link to={'/employees/profile'}>
+            <ListItemButton>
+              <ListItemText primary={'Profile'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding={false}>
+          <Link to={'/'}>
+            <ListItemButton onClick={logOut}>
+              <ListItemText primary={'Log Out'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+      </List>
+      <Divider />
+      {/* <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List> */}
+    </Box>
   );
+
+  const listAdm = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+          <ListItem key={text} disablePadding={false}>
+            <ListItemButton>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))} */}
+        <ListItem disablePadding={false}>
+          <Link to={'/'}>
+            <ListItemButton>
+              <ListItemText primary={'Home'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding={false}>
+          <Link to={'/admins/profile'}>
+            <ListItemButton>
+              <ListItemText primary={'Profile'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to={'/'}>
+            <ListItemButton onClick={logOut}>
+              <ListItemText primary={'Log Out'} />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+      </List>
+      <Divider />
+      {/* <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List> */}
+    </Box>
+  );
+
+  if (!role) {
+    return (
+      <div>
+        {['left'].map((anchor) => (
+          <React.Fragment key={anchor}>
+            {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
+            <IconButton
+              onClick={toggleDrawer(anchor, true)}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+              {listWithNoRole(anchor)}
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  } else if (role === 'EMPLOYEE') {
+    return (
+      <div>
+        {['left'].map((anchor) => (
+          <React.Fragment key={anchor}>
+            {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
+            <IconButton
+              onClick={toggleDrawer(anchor, true)}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+              {listEmp(anchor)}
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  } else if (role === 'ADMIN') {
+    return (
+      <div>
+        {['left'].map((anchor) => (
+          <React.Fragment key={anchor}>
+            {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
+            <IconButton
+              onClick={toggleDrawer(anchor, true)}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+              {listAdm(anchor)}
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  }
 }
+
+export default withRouter(TemporaryDrawer);
--- src/Components/Shared/Sidebar/index.jsx
+++ src/Components/Shared/Sidebar/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from '../Sidebar/sidebar.module.css';
+import { Link, withRouter } from 'react-router-dom';
 import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import * as authActions from 'redux/auth/actions';
+import * as currentUserActions from 'redux/currentUser/actions';
 
 const Sidebar = ({ children }) => {
-  const history = useHistory();
+  const dispatch = useDispatch();
   const [isOpen, setIsOpen] = useState(false);
+  const logOut = () => {
+    dispatch(authActions.setAuthentication(false));
+    dispatch(currentUserActions.setCurrentUserToInitialState());
+  };
+  const role = useSelector((state) => state.auth.authenticated?.role);
   const handleClose = () => {
     setIsOpen(false);
   };
@@ -15,22 +23,76 @@ const Sidebar = ({ children }) => {
         <i className="fa-solid fa-bars"></i>
       </button>
     );
+  } else if (!role)
+    return (
+      <div className={styles.Sidebar}>
+        <section className={styles.container}>
+          <div className={styles.sidebarTitle}>
+            <p>Menu</p>
+            <i className="fa-solid fa-xmark" onClick={() => handleClose()}></i>
+          </div>
+          <Link className={styles.sidebarLink} to="/">
+            Home
+          </Link>
+          <Link className={styles.sidebarLink} to="/signup">
+            Sign-up
+          </Link>
+          <Link className={styles.sidebarLink} to="/login">
+            Log-in
+          </Link>
+        </section>
+        <section className={styles.container}>{children}</section>
+      </div>
+    );
+  else if (role === 'EMPLOYEE') {
+    return (
+      <div className={styles.Sidebar}>
+        <section className={styles.container}>
+          <div className={styles.sidebarTitle}>
+            <p>Menu</p>
+            <i className="fa-solid fa-xmark" onClick={() => handleClose()}></i>
+          </div>
+          <Link className={styles.sidebarLink} to="/">
+            Home
+          </Link>
+          <Link className={styles.sidebarLink} to="/employees/profile">
+            Profile
+          </Link>
+          <Link className={styles.sidebarLink} onClick={logOut} to="/">
+            Log Out
+          </Link>
+        </section>
+        <section className={styles.container}>{children}</section>
+      </div>
+    );
+  } else if (role === 'ADMIN') {
+    return (
+      <div className={styles.Sidebar}>
+        <section className={styles.container}>
+          <div className={styles.sidebarTitle}>
+            <p>Menu</p>
+            <i className="fa-solid fa-xmark" onClick={() => handleClose()}></i>
+          </div>
+          <Link className={styles.sidebarLink} to="/">
+            Home
+          </Link>
+          <Link className={styles.sidebarLink} to="/signup">
+            Sign-up
+          </Link>
+          <Link className={styles.sidebarLink} to="/login">
+            Log-in
+          </Link>
+          <Link className={styles.sidebarLink} to="/admins/profile">
+            Profile
+          </Link>
+          <Link className={styles.sidebarLink} onClick={logOut} to="/">
+            Log Out
+          </Link>
+        </section>
+        <section className={styles.container}>{children}</section>
+      </div>
+    );
   }
-  return (
-    <div className={styles.Sidebar}>
-      <section className={styles.container}>
-        <div className={styles.sidebarTitle}>
-          <p>Menu</p>
-          <i className="fa-solid fa-xmark" onClick={() => handleClose()}></i>
-        </div>
-        <a>Home</a> {/*<Link to="/tasks">home</Link>*/}
-        <a onClick={() => history.push('/signup')}>Sign-up</a>
-        <a onClick={() => history.push('/login')}>Log-in</a>
-        <a>Contact</a>
-      </section>
-      <section className={styles.container}>{children}</section>
-    </div>
-  );
 };
 
 //It must be implemented inside the components indexes
@@ -38,4 +100,4 @@ const Sidebar = ({ children }) => {
 //isOpen is a boolean to indicate if is open or not
 //handleClose should change isOpen to false
 
-export default Sidebar;
+export default withRouter(Sidebar);
