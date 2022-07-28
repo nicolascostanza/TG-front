45962e40f642d7a10c0fa2bca3d6aefbbc910461
Federico Troanes <ftroanes@gmail.com>
2022-07-28 19:26:47 -0300

    TG-177: landing quedando facherita

--- src/Components/Shared/Landing/index.jsx
+++ src/Components/Shared/Landing/index.jsx
@@ -12,19 +12,18 @@ function Landing() {
       <Sidebar />
       <div className={styles.sidebarMain}>
         <main className={styles.mainSection}>
-        <section className={styles.firstSection}>
+          <section className={styles.firstSection}>
             <div className={styles.imgTablet}>
               <div className={styles.divSeparator1}></div>
               <div className={styles.divSeparator2}>
                 <h1 id="trackgenix" className={styles.tittle}>
                   Hi, we are Trackgenix SA
                 </h1>
-                <p className={styles.subtittle}>A FULLY RESPONSIVE SITE</p>
+                <p className={styles.subtittle}>THE MOST POPULAR TEAM TIME TRACKER</p>
                 <p className={styles.textMainSection}>
-                  Sed varius nibh scelerisque tortor sapien. Sit egestas ac imperdiet lorem tempor
-                  malesuada. Massa enim interdum morbi nunc eleifend viverra. Mauris iaculis Mauris
-                  iaculis arcu platea ut massa ultrices. In est porta etiam a nisl quisque Interdum
-                  vel, pellentesque imperdiet lacus fermentum. Consectetur amet, purus leo nulla.
+                  Time tracking software used by millions. Clockify is a time tracker and schedule
+                  app that allows you to track work hours across projects. Unlimited users, free
+                  forever.
                 </p>
               </div>
               {/* <img className={styles.imgTabletSize} src={mainImg} alt="Reloj" /> */}
@@ -35,57 +34,57 @@ function Landing() {
           </h2>
           <section className={styles.secondSection}>
             <div className={styles.peerSeparator}>
-              <div className={styles.group}>
-                <div className={styles.imagenLeftBox}>
-                  <img src={img1} alt="trackgenix" id={styles['descriptionImage']} />
-                  <div className={styles.subGroup}>
-                    <h3 className={styles.groupTitle}>WORKED HOURS RECORD</h3>
-                    <p className={styles.text}>
-                      This app has a lot of hours for each member of the company, so you can then
-                      accept, reject or edit them to your liking to optimize work time
-                    </p>
-                  </div>
+              {/* <div className={styles.group}> */}
+              <div className={styles.imagenLeftBox}>
+                <img src={img1} alt="trackgenix" id={styles['descriptionImage']} />
+                <div className={styles.subGroup}>
+                  <h3 className={styles.groupTitle}>WORKED HOURS RECORD</h3>
+                  <p className={styles.text}>
+                    This app has a lot of hours for each member of the company, so you can then
+                    accept, reject or edit them to your liking to optimize work time
+                  </p>
                 </div>
               </div>
-              <div className={styles.group}>
-                <div className={styles.imagenLeftBox}>
-                  <img src={img2} alt="trackgenix" id={styles['descriptionImage']} />
-                  <div className={styles.subGroup}>
-                    <h3 className={styles.groupTitle}>REPORTS</h3>
-                    <p className={styles.text}>
-                      You will have a section where you will find the reports of all areas of your
-                      company, both in the form of graphics and text. they are useful to detect
-                      possible opportunities
-                    </p>
-                  </div>
+              {/* </div> */}
+              {/* <div className={styles.group}> */}
+              <div className={styles.imagenLeftBox}>
+                <img src={img2} alt="trackgenix" id={styles['descriptionImage']} />
+                <div className={styles.subGroup}>
+                  <h3 className={styles.groupTitle}>REPORTS</h3>
+                  <p className={styles.text}>
+                    You will have a section where you will find the reports of all areas of your
+                    company, both in the form of graphics and text. they are useful to detect
+                    possible opportunities
+                  </p>
                 </div>
               </div>
+              {/* </div> */}
             </div>
             <div className={styles.peerSeparator}>
-              <div className={styles.group}>
-                <div className={styles.imagenLeftBox}>
-                  <img src={img3} alt="trackgenix" id={styles['descriptionImage']} />
-                  <div className={styles.subGroup}>
-                    <h3 className={styles.groupTitle}>RESOURCES MANAGEMENT</h3>
-                    <p className={styles.text}>
-                      You will be able to manage your expenses, costs, salaries and all kinds of
-                      economic movement of your company to make the best possible decisions
-                    </p>
-                  </div>
+              {/* <div className={styles.group}> */}
+              <div className={styles.imagenLeftBox}>
+                <img src={img3} alt="trackgenix" id={styles['descriptionImage']} />
+                <div className={styles.subGroup}>
+                  <h3 className={styles.groupTitle}>RESOURCES MANAGEMENT</h3>
+                  <p className={styles.text}>
+                    You will be able to manage your expenses, costs, salaries and all kinds of
+                    economic movement of your company to make the best possible decisions
+                  </p>
                 </div>
               </div>
-              <div className={styles.group}>
-                <div className={styles.imagenLeftBox}>
-                  <img src={img4} alt="trackgenix" id={styles['descriptionImage']} />
-                  <div className={styles.subGroup}>
-                    <h3 className={styles.groupTitle}>MULTIPLE ROLES</h3>
-                    <p className={styles.text}>
-                      You will have a list of all the members of the company with their respective
-                      roles and functions, which you can add, delete and change their role.
-                    </p>
-                  </div>
+              {/* </div> */}
+              {/* <div className={styles.group}> */}
+              <div className={styles.imagenLeftBox}>
+                <img src={img4} alt="trackgenix" id={styles['descriptionImage']} />
+                <div className={styles.subGroup}>
+                  <h3 className={styles.groupTitle}>MULTIPLE ROLES</h3>
+                  <p className={styles.text}>
+                    You will have a list of all the members of the company with their respective
+                    roles and functions, which you can add, delete and change their role.
+                  </p>
                 </div>
               </div>
+              {/* </div> */}
             </div>
           </section>
           <div className={styles.separator}></div>
@@ -94,59 +93,59 @@ function Landing() {
           </h2>
           <section className={styles.secondSection}>
             <div className={styles.peerSeparator}>
-              <div className={styles.group}>
-                <div className={styles.imagenLeftBox}>
-                  <div className={styles.descriptionImage}>
-                    <img src={img1} alt="trackgenix" id={styles['descriptionImage']} />
-                  </div>
-                  <div className={styles.subGroup}>
-                    <h3 className={styles.groupTitle}>PRODUCTIVITY ENHANCER</h3>
-                    <p className={styles.text}>
-                      This app has a lot of hours for each member of the company, so you can then
-                      accept, reject or edit them to your liking to optimize work time
-                    </p>
-                  </div>
+              {/* <div className={styles.group}> */}
+              <div className={styles.imagenLeftBox}>
+                <div className={styles.descriptionImage}>
+                  <img src={img1} alt="trackgenix" id={styles['descriptionImage']} />
+                </div>
+                <div className={styles.subGroup}>
+                  <h3 className={styles.groupTitle}>PRODUCTIVITY ENHANCER</h3>
+                  <p className={styles.text}>
+                    This app has a lot of hours for each member of the company, so you can then
+                    accept, reject or edit them to your liking to optimize work time
+                  </p>
                 </div>
               </div>
-              <div className={styles.group}>
-                <div className={styles.imagenLeftBox}>
-                  <img src={img2} alt="trackgenix" id={styles['descriptionImage']} />
-                  <div className={styles.subGroup}>
-                    <h3 className={styles.groupTitle}>DECISION MAKING</h3>
-                    <p className={styles.text}>
-                      You will have a section where you will find the reports of all areas of your
-                      company, both in the form of graphics and text. they are useful to detect
-                      possible opportunities
-                    </p>
-                  </div>
+              {/* </div> */}
+              {/* <div className={styles.group}> */}
+              <div className={styles.imagenLeftBox}>
+                <img src={img2} alt="trackgenix" id={styles['descriptionImage']} />
+                <div className={styles.subGroup}>
+                  <h3 className={styles.groupTitle}>DECISION MAKING</h3>
+                  <p className={styles.text}>
+                    You will have a section where you will find the reports of all areas of your
+                    company, both in the form of graphics and text. they are useful to detect
+                    possible opportunities
+                  </p>
                 </div>
               </div>
+              {/* </div> */}
             </div>
             <div className={styles.peerSeparator}>
-              <div className={styles.group}>
-                <div className={styles.imagenLeftBox}>
-                  <img src={img3} alt="trackgenix" id={styles['descriptionImage']} />
-                  <div className={styles.subGroup}>
-                    <h3 className={styles.groupTitle}>WORK TRACEABILITY</h3>
-                    <p className={styles.text}>
-                      You will be able to manage your expenses, costs, salaries and all kinds of
-                      economic movement of your company to make the best possible decisions
-                    </p>
-                  </div>
+              {/* <div className={styles.group}> */}
+              <div className={styles.imagenLeftBox}>
+                <img src={img3} alt="trackgenix" id={styles['descriptionImage']} />
+                <div className={styles.subGroup}>
+                  <h3 className={styles.groupTitle}>WORK TRACEABILITY</h3>
+                  <p className={styles.text}>
+                    You will be able to manage your expenses, costs, salaries and all kinds of
+                    economic movement of your company to make the best possible decisions
+                  </p>
                 </div>
               </div>
-              <div className={styles.group}>
-                <div className={styles.imagenLeftBox}>
-                  <img src={img4} alt="trackgenix" id={styles['descriptionImage']} />
-                  <div className={styles.subGroup}>
-                    <h3 className={styles.groupTitle}>LEADERSHIP AND TEAM MANAGEMENT</h3>
-                    <p className={styles.text}>
-                      You will have a list of all the members of the company with their respective
-                      roles and functions, which you can add, delete and change their role.
-                    </p>
-                  </div>
+              {/* </div> */}
+              {/* <div className={styles.group}> */}
+              <div className={styles.imagenLeftBox}>
+                <img src={img4} alt="trackgenix" id={styles['descriptionImage']} />
+                <div className={styles.subGroup}>
+                  <h3 className={styles.groupTitle}>LEADERSHIP AND TEAM MANAGEMENT</h3>
+                  <p className={styles.text}>
+                    You will have a list of all the members of the company with their respective
+                    roles and functions, which you can add, delete and change their role.
+                  </p>
                 </div>
               </div>
+              {/* </div> */}
             </div>
           </section>
           <div className={styles.separator}></div>
